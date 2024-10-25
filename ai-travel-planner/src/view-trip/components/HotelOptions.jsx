ee74ce4f53b8382da5082a373a94a0a64f8c19e8
Lakshitha Vimuth <lvimuthfb@gmail.com>
2024-10-26 00:42:16 +0400

    Hotel options

--- ai-travel-planner/src/view-trip/components/HotelOptions.jsx
+++ ai-travel-planner/src/view-trip/components/HotelOptions.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
 
-function HotelOptions({trip}) {
+function HotelOptions({ trip }) {
   return (
-    <div>HotelOptions</div>
-  )
+    <div>
+      <h2 className="font-bold text-xl mt-5">Hotel Recommendation</h2>
+      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+        {console.log(trip?.tripData?.hotel_options)}
+        {trip?.tripData?.hotel_options?.map((item, index) => (
+          <div>
+            <img src="/placeholder.png" alt="" className="rounded-xl" />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default HotelOptions
+export default HotelOptions;
